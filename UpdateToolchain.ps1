b0c8b779e0205c824b185636d129729b418d9c0a
Robin Jones <networkfusion@users.noreply.github.com>
2020-11-14 23:32:13 +0000

    Use official ninja (now it works with GCC 10.1)

--- UpdateToolchain.ps1
+++ UpdateToolchain.ps1
@@ -12,4 +12,10 @@ $output = "$PSScriptRoot\temp\gcc-toolchain-mips64.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Force -Path $output -DestinationPath "$PSScriptRoot\toolchain\gcc-toolchain-mips64\"
 
+$url = "https://github.com/ninja-build/ninja/releases/download/v1.10.1/ninja-win.zip"
+$output = "$PSScriptRoot\temp\ninja-win.zip"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Expand-Archive -Force -Path $output -DestinationPath "$PSScriptRoot\tools\"
+
 Remove-Item -LiteralPath "$PSScriptRoot\temp\" -Force -Recurse
