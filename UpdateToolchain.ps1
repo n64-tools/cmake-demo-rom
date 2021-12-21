f2eb68b5fce4b4550ee5705220b1e96a3102a2ba
Robin Jones <networkfusion@users.noreply.github.com>
2021-12-21 20:19:23 +0000

    Update UpdateToolchain.ps1
    Fix download locations

--- UpdateToolchain.ps1
+++ UpdateToolchain.ps1
@@ -1,15 +1,21 @@
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\temp\"
 
-$url = "https://n64tools.blob.core.windows.net/binaries/N64-tools/libdragon/develop-displaytest/latest/libdragon-win64.zip"
+$url = "https://github.com/N64-tools/libdragon/releases/download/v0.0.1-alpha.1/libdragon-win64.zip"
 $output = "$PSScriptRoot\temp\libdragon.zip"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Force -Path $output -DestinationPath "$PSScriptRoot\toolchain\libdragon\"
 
-$url = "https://n64tools.blob.core.windows.net/binaries/N64-tools/mips64-gcc-toolchain/master/latest/gcc-toolchain-mips64-win64.zip"
+$url = "https://github.com/N64-tools/mips64-gcc-toolchain/releases/download/10.2.0/gcc-toolchain-mips64-win64.zip"
 $output = "$PSScriptRoot\temp\gcc-toolchain-mips64.zip"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Expand-Archive -Force -Path $output -DestinationPath "$PSScriptRoot\toolchain\gcc-toolchain-mips64\"
+
+$url = "https://github.com/ninja-build/ninja/releases/latest/download/ninja-win.zip"
+$output = "$PSScriptRoot\temp\ninja.zip"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Expand-Archive -Force -Path $output -DestinationPath "$PSScriptRoot\tools\"
 
 Remove-Item -LiteralPath "$PSScriptRoot\temp\" -Force -Recurse
